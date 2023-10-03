49e5dec8e83687be31dd9f9c4748537a3536b2bb
Tiamiyu Muizz <48317927+tiammui@users.noreply.github.com>
2023-10-04 00:46:05 +0100

    just commiting

--- app.py
+++ app.py
@@ -1,35 +1,91 @@
-from flask import Flask
+from flask import Flask, render_template, request
 from zk import ZK, const
+from datetime import datetime
 
-zk = ZK('192.168.1.201', port=4370, timeout=5, password=0, force_udp=False, ommit_ping=False)
+zk = ZK(
+    "192.168.1.201", port=4370, timeout=5, password=0, force_udp=False, ommit_ping=False
+)
 
 app = Flask(__name__)
 
+conn = zk.connect()
+conn.set_time(datetime.now())
+conn.disconnect()
+
+
 @app.route("/")
-def hello_world():
-        # connect to device
+def index():
+    return render_template("index.html")
+
+@app.route("/users")
+def users():
+    # connect to device
     conn = zk.connect()
     # disable device, this method ensures no activity on the device while the process is run
     conn.disable_device()
     # another commands will be here!
     # Example: Get All Users
     users = conn.get_users()
-    nser = []
+    userList = []
     for user in users:
-        privilege = 'User'
+        privilege = "User"
         if user.privilege == const.USER_ADMIN:
-            privilege = 'Admin'
-        print ('+ UID #{}'.format(user.uid))
-        print ('  Name       : {}'.format(user.name))
-        print ('  Privilege  : {}'.format(privilege))
-        print ('  Password   : {}'.format(user.password))
-        print ('  Group ID   : {}'.format(user.group_id))
-        print ('  User  ID   : {}'.format(user.user_id))
-        nser.append({'uid':user.name})
+            privilege = "Admin"
 
-    # Test Voice: Say Thank You
-    conn.test_voice(index=8)
+        userList.append({"name":user.name,"uid": user.uid,"userID":user.user_id,"department":user.group_id})
+
     # re-enable device after all commands already executed
     conn.enable_device()
 
-    return nser
+    return userList
+
+
+@app.route("/attendances")
+def attendances():
+    # connect to device
+    conn = zk.connect()
+    # disable device, this method ensures no activity on the device while the process is run
+    conn.disable_device()
+    # another commands will be here!
+    # Example: Get All Users
+    attends = conn.get_attendance()
+    attList = []
+    for att in attends:
+        attList.append({"status": att.status,"timestamp": datetime.timestamp(att.timestamp), "userID":att.user_id})
+
+    # re-enable device after all commands already executed
+    conn.enable_device()
+
+    return attList
+
+
+@app.route("/connectStatus")
+def connect_status():
+    # connect to device
+    conn = zk.connect()
+    # disable device, this method ensures no activity on the device while the process is run
+    conn.disable_device()
+
+
+@app.route("/enroll")
+def enroll():
+    # connect to device
+    conn = zk.connect()
+    # disable device, this method ensures no activity on the device while the process is run
+    conn.disable_device()
+
+@app.post("/user")
+def user():
+    # connect to device
+    conn = zk.connect()
+    # disable device, this method ensures no activity on the device while the process is run
+    conn.disable_device()
+    
+    body = request.json
+
+    conn.
+
+    # re-enable device after all commands already executed
+    conn.enable_device()
+
+    return body
